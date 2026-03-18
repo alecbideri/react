3cb243da94cc769706efde369d3272bff2dc010d
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2026-03-18 10:09:59 +0200

    project 1

---
+++ react-monster/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div style={{backgroundColor: 'black' , minHeight: '100vh' , width: '100%'}}>
+          <div style={{color : 'white'}}>
+            app
+          </div>
+        </div>
+    )
+}
+export default App
